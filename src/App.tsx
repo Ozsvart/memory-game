2db5f75c58511f0e6ed91e2ba8dda3e81ba523b7
Ozsvart <v.ozsvart@gmail.com>
2024-12-29 00:11:48 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import GameBoard from "./Game/GameBoard";
+
+function App() {
+  return (
+    <div>
+      <GameBoard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Game/GameBoard.tsx
@@ -0,0 +1,9 @@
+function GameBoard() {
+  return (
+    <div>
+      <h1>This is GameBoard!</h1>T
+    </div>
+  );
+}
+
+export default GameBoard;
