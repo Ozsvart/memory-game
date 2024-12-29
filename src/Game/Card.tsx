4bf0e4b5b8fb9afd36924f7b4876b3c52239792d
Ozsvart <v.ozsvart@gmail.com>
2024-12-29 22:52:11 +0100

    card and data

--- src/Game/Card.tsx
+++ src/Game/Card.tsx
@@ -1,3 +1,25 @@
-export default function Card() {
-  return <div>This is Card</div>;
+export default function Card({
+  item,
+  handleSelectedCards,
+  toggled,
+  stopflip,
+}: {
+  item: { id: number; name: string; img: string; matched: boolean };
+  handleSelectedCards: (item: object) => void;
+  toggled: boolean;
+  stopflip: boolean;
+}) {
+  return (
+    <div className="item">
+      <div className={toggled ? "toggled" : ""}>
+        <img className="face" src={item.img} alt="face" />
+        <div
+          className="back"
+          onClick={() => !stopflip && handleSelectedCards(item)}
+        >
+          {" "}
+        </div>
+      </div>
+    </div>
+  );
 }
